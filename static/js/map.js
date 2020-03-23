0df42bb65af5987cd9b2d3271fcaf4315eae59bc
Izhan Alam <izhanalam@hotmail.com>
2020-03-23 03:03:45 -0700

    FInal javascript used

--- static/js/map.js
+++ static/js/map.js
@@ -9,10 +9,10 @@ function get_data_from_url(url){
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg';
 var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-  center: [-114.0719,51.0447], // starting position
-  zoom: 12 // starting zoom
+  container: 'map', 
+  style: 'mapbox://styles/izhanalam/ck846sjfv199t1io8gd7v2h9u', 
+  center: [-114.0719,51.0447],
+  zoom: 10
 });
 
 var school_json = 'https://data.calgary.ca/resource/fd9t-tdn2.geojson'
@@ -59,7 +59,7 @@ map.on('load', function() {
       data: school
     },
     layout: {
-      'icon-image': 'library-15'
+      'icon-image': 'school-11'
     },
     paint: { }
   });
@@ -93,16 +93,20 @@ map.on('mousemove', function(e) {
 
 });
 
+
+
+
 map.on('click', function(e) {
-  var libraryFeatures = map.queryRenderedFeatures(e.point, { layers: ['school'] });
-  if (!libraryFeatures.length) {
+
+  var schoolFeatures = map.queryRenderedFeatures(e.point, { layers: ['school'] });
+  if (!schoolFeatures.length) {
     return;
   }
 
-  var libraryFeature = libraryFeatures[0];
+  var schoolFeature = schoolFeatures[0];
 
-  var nearestHospital = turf.nearest(libraryFeature, hospitals);
-
+  var nearestHospital = turf.nearest(schoolFeature, hospitals);
+  
   if (nearestHospital !== null) {
 
     map.getSource('nearest-hospital').setData({
@@ -116,276 +120,15 @@ map.on('click', function(e) {
       source: 'nearest-hospital',
       paint: {
         'circle-radius': 12,
-        'circle-color': '#486DE0'
+        'circle-color': 'black'
       }
     }, 'hospitals');
   }
-});
 
-
-/*
-var map = L.map('mapid').setView([51.0447, -114.0719], 11);
-
-
-
-var blackIcon = new L.Icon({
-  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-
-//Schools
-var featureGroup = L.featureGroup();
-var geojsonLayer = L.geoJSON().addTo(map);
-var markers = L.markerClusterGroup();
-//Hospitals
-var featureGroup2 = L.featureGroup();
-var geojsonLayer2 = L.geoJSON().addTo(map);
-var markers2 = L.markerClusterGroup();
-//Clinics
-var featureGroup3 = L.featureGroup();
-var geojsonLayer3 = L.geoJSON().addTo(map);
-var markers3 = L.markerClusterGroup();
-
-
-var layers = document.getElementById('menu-ui');
-
-
-
-
-
-
-
-
-var layer1 = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/izhanalam/ck7ty90g50vzr1is1dmhhug57/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg',  {
-      tileSize: 512,
-      zoomOffset: -1,
-      attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
-
-
-
-var layer2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg'
-}).addTo(map);
-
-
-//https://api.mapbox.com/styles/v1/izhanalam/ck7u70ax90z021ipamn7ndflk.html?fresh=true&title=view&access_token=pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg
-var layer3 = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/izhanalam/ck7u70ax90z021ipamn7ndflk/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg',  {
-      tileSize: 512,
-      zoomOffset: -1,
-      attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
-//https://api.mapbox.com/styles/v1/izhanalam/ck7uaqhpi0tva1is5xucpnar3.html?fresh=true&title=view&access_token=pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg
-var layer4 = L.tileLayer(
-    'https://api.mapbox.com/styles/v1/izhanalam/ck7uaqhpi0tva1is5xucpnar3/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaXpoYW5hbGFtIiwiYSI6ImNrN2s2ZDFiaTAzbzgzZG11MG1xMHdlZzEifQ.uBG-TBw1B2h81lSwJcLPvg',  {
-        tileSize: 512,
-        zoomOffset: -1,
-        attribution: '© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-
-
-
-//Schools
-
-//Create the dropdown menu
-$(function(){
-  
-  
-
-  var school_json = 'https://data.calgary.ca/resource/fd9t-tdn2.json'
-  var school_obj = JSON.parse(get_data_from_url(school_json));
-
-  $.each(school_obj, function(i, option) {
-    $('#School_name').append($('<option/>').attr("value", option.name).text(option.name));
-    
-  });
- 
-
-  
+  var school_name = schoolFeature.properties.name;
+  var hospital_name = nearestHospital.properties.name;
+  document.getElementById("schoolname").innerHTML = school_name;
+  document.getElementById("hospitalname").innerHTML = hospital_name;
 
 });
-
-//Get the selection
-$('#School_name').click(function(){ 
-  var school_name = $(this).val();
-  var nparams = "?name=" + school_name;
-
-
-  var school_json = 'https://data.calgary.ca/resource/fd9t-tdn2.geojson' + nparams;
-  var school_obj = JSON.parse(get_data_from_url(school_json));
-
-
-  markers.clearLayers();
-
-  geojsonLayer = L.geoJson(school_obj, {
-    
-    onEachFeature: function(feature,layer){
-      layer.bindPopup("<p>Issued Date: <p>" + feature.properties.issueddate + "<br>" + "<p>Community Name: <p>" + feature.properties.communityname + "<br>" + "<p>Work Class Group: <p>" + feature.properties.workclassgroup + "<br>" + "<p>Contractor Name: <p>" + feature.properties.contractorname + "<br>" + "<p>Original Address: <p>" + feature.properties.originaladdress).openPopup;
-    }
-    });
-  markers.addLayer(geojsonLayer);
-  featureGroup.addLayer(markers);
-  featureGroup.addTo(map);
-
-});
-
-// Hospitals
-
-$(function(){
-  
-
-  var hospital_json = 'https://data.calgary.ca/resource/x34e-bcjz.json?type=Hospital';
-  var hospital_obj = JSON.parse(get_data_from_url(hospital_json));
-
-  $.each(hospital_obj, function(i, option) {
-    $('#hosp_name').append($('<option/>').attr("value", option.name).text(option.name));
-    
-  });
- 
-
-  
-
-});
-
-//Get the selection
-$('#hosp_name').click(function(){ 
-  var hospital_name = $(this).val();
-  
-
-
-  var hospital_json = 'https://data.calgary.ca/resource/x34e-bcjz.geojson?type=Hospital&name=' + hospital_name;
-  var hospital_obj = JSON.parse(get_data_from_url(hospital_json));
-
-  geojsonLayer2 = L.geoJson(hospital_obj, {
-    pointToLayer: function (feature, latlng){
-      return L.marker(latlng, {icon: blackIcon});
-    },
-    onEachFeature: function(feature,layer){
-      layer.bindPopup("<p>Issued Date: <p>" + feature.properties.issueddate + "<br>" + "<p>Community Name: <p>" + feature.properties.communityname + "<br>" + "<p>Work Class Group: <p>" + feature.properties.workclassgroup + "<br>" + "<p>Contractor Name: <p>" + feature.properties.contractorname + "<br>" + "<p>Original Address: <p>" + feature.properties.originaladdress).openPopup;
-    }
-    });
-
-
-  clear_markers2();  
-  markers2.addLayer(geojsonLayer2);
-  featureGroup2.addLayer(markers2);
-  featureGroup2.addTo(map);
-
-});
-
-
-//Clinic
-
-$(function(){
-  
-  
-
-  var clinic_json = 'https://data.calgary.ca/resource/x34e-bcjz.json?type=PHS%20Clinic';
-  var clinic_obj = JSON.parse(get_data_from_url(clinic_json));
-
-  $.each(clinic_obj, function(i, option) {
-    $('#clinic_name').append($('<option/>').attr("value", option.name).text(option.name));
-    
-  });
- 
-
-  
-
-});
-
-//Get the selection
-$('#clinic_name').click(function(){ 
-  var clinic_name = $(this).val();
-
-  var clinic_json = 'https://data.calgary.ca/resource/x34e-bcjz.geojson?type=PHS%20Clinic&name=' + clinic_name;
-  var clinic_obj = JSON.parse(get_data_from_url(clinic_json));
-
-  geojsonLayer3 = L.geoJson(clinic_obj, {
-    pointToLayer: function (feature, latlng){
-      return L.marker(latlng, {icon: blackIcon});
-    },
-    onEachFeature: function(feature,layer){
-      layer.bindPopup("<p>Issued Date: <p>" + feature.properties.issueddate + "<br>" + "<p>Community Name: <p>" + feature.properties.communityname + "<br>" + "<p>Work Class Group: <p>" + feature.properties.workclassgroup + "<br>" + "<p>Contractor Name: <p>" + feature.properties.contractorname + "<br>" + "<p>Original Address: <p>" + feature.properties.originaladdress).openPopup;
-    }
-    });
-
-
-  clear_markers3();  
-  markers3.addLayer(geojsonLayer3);
-  featureGroup3.addLayer(markers3);
-  featureGroup3.addTo(map);
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-//--------------------------------------------------
-function add_layer(layer){
-
-  map.removeLayer(layer1);
-  map.removeLayer(layer2);
-  map.removeLayer(layer3);
-  map.removeLayer(layer4);
-
-
-  if (map.hasLayer(layer)){
-    map.removeLayer(layer);
-  }
-  else {
-    map.addLayer(layer);
-  }
-};
-
-
-function clear_layers(){
-map.clearLayers();
-};
-
-
-function clear_markers(){
-markers.clearLayers();
-markers2.clearLayers();
-markers3.clearLayers();
-};
-
-
-function clear_marker1(){
-  markers.clearLayers();
-};
-function clear_markers2(){
-
-  markers2.clearLayers();
-};
-function clear_markers3(){
-
-  markers3.clearLayers();
-};
-
-
-
-//----------------------------------------------
-
-
-*/
 
